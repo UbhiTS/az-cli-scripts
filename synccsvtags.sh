74dc20f1f8596493971ab340717c698ceb53f3e5
Tarunpreet Ubhi <ubhits@outlook.com>
2021-04-07 21:22:54 -0700

    initial commit

---
+++ synccsvtags.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+#   usage: bash synccsvtags.sh <csv_file> <azure_subscription_id>
+# example: bash synccsvtags.sh tags.csv 11111111-1111-1111-1111-111111111111
+
+lines=()
+
+# read file
+while IFS= read -r x
+do
+    lines+=("$x")
+done < $1
+
+tagnames=()
+
+# split into lines
+for line in "${!lines[@]}"
+do
+
+    IFS=',' read -r -a tags <<< "${lines[line]}"
+
+    # get headers
+    if [[ line -eq 0 ]]
+    then
+        tagnames=("${tags[@]}")
+        continue
+    fi
+
+    new_tags=""
+
+    # get tag values
+    for index in "${!tags[@]}"
+    do
+
+        # skip resource name column
+        if [[ index -eq 0 ]]
+        then
+            continue
+        fi
+
+        new_tags="${new_tags} ${tagnames[index]}=${tags[index]}"
+        
+    done
+
+    #echo "$line : ${tags[0]} ${new_tags}"
+
+    azclicommand="az tag create --resource-id /subscriptions/$2/resourcegroups/${tags[0]} --tags ${new_tags}"
+    echo $azclicommand
+    #eval $azclicommand
+    echo
+
+done
